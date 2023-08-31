cc35de0a0ab8400e335031072200d6fd3de51638
Julian Salazar <juliansalazarvelez94@gmail.com>
2023-08-31 13:05:49 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Puedes añadir interacciones aquí si lo deseas
+// Por ejemplo, mostrar un mensaje al hacer clic en un botón
+
+// Ejemplo:
+const button = document.querySelector('#contactButton');
+button.addEventListener('click', () => {
+    alert('¡Gracias por contactarnos! Te responderemos pronto.');
+});
